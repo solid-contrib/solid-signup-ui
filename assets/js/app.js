af2718934eb3756b048fd38c4e4d49555bd41954
Guillaume Baudusseau <guillaume.baudusseau@gmail.com>
2016-05-05 16:06:38 -0400

    clean onboarding code

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,32 @@
 
     var swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
-        paginationClickable: false
+        paginationClickable: true
     });
 
-    var nextBtn = document.getElementById('next-button');
+    // get elements
+    var onboarding = document.getElementById('onboarding');
+    var nextBtn = document.getElementById('control-btn-next');
+    var skipBtn = document.getElementById('control-btn-skip');
+    var onboardingControlPanel = document.getElementById('onboarding-control-panel');
+
+    // add event listeners
     nextBtn.addEventListener('click', function() {
-        console.log('qwer');
         swiper.slideNext(false, 300);
-    }, true);
+    }, false);
+
+    skipBtn.addEventListener('click', function() {
+        onboarding.style.display = 'none';
+    }, false);
+
+    // detect end of onboarding
+    swiper.on('ReachEnd', function () {
+        onboardingControlPanel.innerHTML = '<p class="onboarding-done"><button id="control-btn-done" class="control-btn" type="button" name="done">got it</button></p>';
+        var doneBtn = document.getElementById('control-btn-done')
+        doneBtn.addEventListener('click', function() {
+            onboarding.style.display = 'none';
+        }, false);
+        doneBtn.style.display = 'block';
+    });
 
 })();
