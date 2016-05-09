a4eeaa1df276ace714bf58177052c6301b28b6c0
Guillaume Baudusseau <guillaume.baudusseau@gmail.com>
2016-05-09 08:29:48 -0400

    new form and new icons for onboarding

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,20 @@
 
     var swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
-        paginationClickable: true
+        paginationClickable: true,
+        //allowSwipeToPrev: false
+    });
+
+    var signupswiper = new Swiper('.signup-swiper-container', {
+        allowSwipeToPrev: false,
+        allowSwipeToNext: false
     });
 
     // get elements
     var onboarding = document.getElementById('onboarding');
     var nextBtn = document.getElementById('control-btn-next');
     var skipBtn = document.getElementById('control-btn-skip');
+    var signupBtn = document.getElementById('sign-up-btn');
     var onboardingControlPanel = document.getElementById('onboarding-control-panel');
 
     // add event listeners
@@ -20,14 +27,19 @@
         onboarding.style.display = 'none';
     }, false);
 
+    signupBtn.addEventListener('click', function() {
+        onboarding.style.display = 'none';
+    }, false);
+
     // detect end of onboarding
     swiper.on('ReachEnd', function () {
-        onboardingControlPanel.innerHTML = '<p class="onboarding-done"><button id="control-btn-done" class="control-btn" type="button" name="done">got it</button></p>';
-        var doneBtn = document.getElementById('control-btn-done')
+        //onboardingControlPanel.innerHTML = '<p class="onboarding-done"><button id="control-btn-done" class="control-btn" type="button" name="done">got it</button></p>';
+        /*var doneBtn = document.getElementById('control-btn-done')
         doneBtn.addEventListener('click', function() {
             onboarding.style.display = 'none';
         }, false);
-        doneBtn.style.display = 'block';
+        doneBtn.style.display = 'block';*/
+        onboardingControlPanel.style.display = 'none';
     });
 
 })();
---
+++ assets/js/app2.js
@@ -0,0 +1,7 @@
+(function() {
+
+    var swiper = new Swiper('.sign-up-swiper-container', {
+        //allowSwipeToPrev: false
+    });
+
+})();
